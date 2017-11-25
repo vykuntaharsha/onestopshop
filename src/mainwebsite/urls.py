ee7041141a23d10edeef5eaa91b38a42e8e14548
Harsha Vykunta <harshavardhan.biit@gmail.com>
2017-11-24 16:17:03 -0800

    created login and registration

--- src/mainwebsite/urls.py
+++ src/mainwebsite/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from .views import home_page, about_page, contact_page
+from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', home_page, name='home'),
     url(r'^about/$', about_page, name='about'),
     url(r'^contact/$', contact_page, name='contact'),
+    url(r'^login/$', login_page, name='login'),
+    url(r'^register/$', register_page, name='register'),
 
 ]
--- src/mainwebsite/views.py
+++ src/mainwebsite/views.py
@@ -1,6 +1,7 @@
+from django.contrib.auth import authenticate, login, get_user_model
 from django.http import HttpResponse
-from django.shortcuts import render
-from .forms import ContactForm
+from django.shortcuts import render, redirect
+from .forms import ContactForm, LoginForm, RegisterForm
 
 
 def home_page(request):
@@ -28,3 +29,37 @@ def contact_page(request):
     }
     return render(request, template_name='contact/view.html',context=context)
 
+
+def login_page(request):
+    login_form = LoginForm(request.POST or None)
+    context = {
+        'form': login_form
+    }
+    if login_form.is_valid():
+        username = login_form.cleaned_data.get('username')
+        password = login_form.cleaned_data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            context['form'] = login_form
+            return redirect('/')
+        else:
+            print('error')
+    return render(request, template_name='auth/login.html', context=context)
+
+
+User = get_user_model()
+
+
+def register_page(request):
+    register_form = RegisterForm()
+    context = {
+        'form': register_form
+    }
+    if register_form.is_valid():
+        username = register_form.cleaned_data.get('username')
+        email = register_form.cleaned_data.get('email')
+        password = register_form.cleaned_data.get('password')
+        new_user = User.objects.create_user(username,email,password)
+        redirect('/login')
+    return render(request, template_name='auth/register.html', context=context)
